440a426598f21ab51abd69853c2772d1f00ff17b
ylgrgyq <alven.gr@gmail.com>
2019-05-10 15:46:40 +0800

    (chore) comments

--- game-hook-base/src/main/java/cn/leancloud/play/hook/request/RoomSystemProperty.java
+++ game-hook-base/src/main/java/cn/leancloud/play/hook/request/RoomSystemProperty.java
@@ -2,11 +2,28 @@ package cn.leancloud.play.hook.request;
 
 import clojure.lang.Keyword;
 
+/**
+ * 房间系统属性接口，所有房间系统属性均会实现。
+ * @param <V> 房间系统属性值类型
+ */
 public interface RoomSystemProperty<V> {
+    /**
+     * 房间属性 Key，Game Server 使用，Game Hook 实现者无需使用
+     *
+     * @return 房间属性 Key
+     */
     Keyword getPropertyKey();
 
+    /**
+     * 房间属性值
+     * @return 房间属性值
+     */
     V getPropertyValueToSet();
 
+    /**
+     * Game Server 内使用的房间属性值，Game Hook 实现者无需使用
+     * @return 房间属性值
+     */
     default Object getSerializedPropertyValue() {
         return getPropertyValueToSet();
     }
