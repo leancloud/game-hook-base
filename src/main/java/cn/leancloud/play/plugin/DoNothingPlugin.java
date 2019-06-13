6210f1bdaac04744b16aeb1e9544aa3577cefe03
ylgrgyq <alven.gr@gmail.com>
2019-06-13 18:01:26 +0800

    (feat) add do nothing plugin

---
+++ src/main/java/cn/leancloud/play/plugin/DoNothingPlugin.java
@@ -0,0 +1,10 @@
+package cn.leancloud.play.plugin;
+
+/**
+ * 什么都不做的 Plugin
+ */
+public class DoNothingPlugin extends AbstractPlugin {
+    public DoNothingPlugin(BoundRoom room) {
+        super(room);
+    }
+}
